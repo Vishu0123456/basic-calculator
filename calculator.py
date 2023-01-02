da766ab6baa1f747bcfce636783f563c65c210e0
Vishwajeet Kumar <115339745+Vishu0123456@users.noreply.github.com>
2023-01-02 22:20:36 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# THIS IS A BASIC CALULATOR WHICH PERFORM ONLY ADDITION . 
+
+'''# create a calculator with the help of two number which helps only to 
+add the numbers.'''
+print("Welcome to the Vishwajeet's calculator")
+print("Enter Your First Number")
+num = input()
+print("Enter Your Second Number")
+num1 = input()
+print("Sum Of These Two Numbers Are : ",int(num)+int(num1))
+print("THANK YOU FOR USING VISHWAJEET'S CALCULATOR")
